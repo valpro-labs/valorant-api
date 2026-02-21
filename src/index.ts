55f633857ca24a0987e2ac52514dff5d18461c0a
rhy3h <rhy3htw@gmail.com>
2026-02-21 16:40:41 +0800

    fix: missing export for endpoints

--- src/index.ts
+++ src/index.ts
@@ -52,6 +52,21 @@ class ValorantApi {
   }
 }
 
-export { ValorantLocale, ValorantApiConfig } from './endpoints/BaseEndpoint';
+export * from './endpoints/AgentsEndpoints';
+export * from './endpoints/BaseEndpoint';
+export * from './endpoints/BuddiesEndpoints';
+export * from './endpoints/BundlesEndpoints';
+export * from './endpoints/CompetitiveTiersEndpoints';
+export * from './endpoints/ContentTierEndpoints';
+export * from './endpoints/CurrenciesEndpoints';
+export * from './endpoints/LevelBordersEndpoints';
+export * from './endpoints/MapsEndpoints';
+export * from './endpoints/MissionsEndpoints';
+export * from './endpoints/ObjectivesEndpoints';
+export * from './endpoints/PlayerCardsEndpoints';
+export * from './endpoints/PlayerTitlesEndpoints';
+export * from './endpoints/SpraysEndpoints';
+export * from './endpoints/VersionEndpoint';
+export * from './endpoints/WeaponsEndpoints';
 
 export { ValorantApi };
