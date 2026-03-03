5d40546fb57dd0dded667eafb605ee31e0ca5746
rhy3h <rhy3htw@gmail.com>
2026-03-03 13:51:22 +0800

    refactor: restructure test files to endpoints subdirectory

--- tests/endpoints/EventsEndpoints.test.ts
+++ tests/endpoints/EventsEndpoints.test.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ValorantApi } from '../src/index';
+import { ValorantApi } from '../../src/index';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
